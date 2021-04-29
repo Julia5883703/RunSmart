4948c06aab5ffed58ec0849ecbb0ac26eeab0dbf
Julia <julia.tsyb@gmail.com>
2021-04-29 21:28:56 +0300

    Promo and advantages sections

--- gulpfile.js
+++ gulpfile.js
@@ -12,28 +12,27 @@ gulp.task('server', function () {
             baseDir: "src"
         }
     });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function () {
-    return gulp.src("src/scss/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError))
         .pipe(rename({
-            prefix: "",
             suffix: ".min",
+            prefix: ""
         }))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
-    gulp.watch("src/scss/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));    
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
